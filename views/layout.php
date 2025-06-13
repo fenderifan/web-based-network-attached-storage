7791bedb08ba92b2a20d62e6be2b2b5f1cd1dc61
Ndeerrr <gifananak@gmail.com>
2025-06-13 07:29:36 +0700

    Bootstrap!!

--- views/layout.php
+++ views/layout.php
@@ -17,8 +17,8 @@ $text_class = $theme === 'dark' ? 'text-light' : 'text-dark';
   <meta charset="UTF-8" />
   <title><?= htmlspecialchars($title) ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+  <link rel="" href="/bootstrap/icons/bootstrap-icons.css" rel="stylesheet"/>
   <style>
     
 html, body {
@@ -657,6 +657,6 @@ document.addEventListener('DOMContentLoaded', () => {
 </script>
 <?php endif; ?>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
